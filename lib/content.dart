d403e195734aeec9ec34c1ab45112d3deaf4f2b0
GavinHome <yangxiaoming512@yeah.net>
2019-11-04 18:03:40 +0800

    add content view page

--- lib/content.dart
+++ lib/content.dart
@@ -31,52 +31,68 @@ class _ContentPageState extends State<ContentPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              margin: EdgeInsets.all(25),
+              width: 340,
+              height: 220,
               decoration: new BoxDecoration(
                   color: const Color(0xff000000),
                   borderRadius:
                       new BorderRadius.all(const Radius.circular(10.0))),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Container(
+                    padding: EdgeInsets.only(top: 18, left: 10, right: 10),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
                           children: <Widget>[
+                            Text('UI Design',
+                                textAlign: TextAlign.start,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .title
+                                    .copyWith(
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromARGB(
+                                            0xFF, 0x5E, 0xCD, 0xFA))),
                             Padding(
-                              padding: EdgeInsets.only(top: 25, bottom: 10),
-                              child: Text('UI Design',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .title
-                                      .copyWith(
-                                          fontWeight: FontWeight.bold,
-                                          color: Color.fromARGB(
-                                              0xFF, 0x5E, 0xCD, 0xFA))),
-                            ),
-                            Text(
-                              'Certificate',
-                              textAlign: TextAlign.left,
-                              style: TextStyle(color: const Color(0xffffffff)),
+                              padding: EdgeInsets.only(top: 6),
+                              child: Text(
+                                'Certificate',
+                                style: TextStyle(
+                                  color: const Color(0xffffffff),
+                                ),
+                              ),
                             ),
                           ],
                         ),
-                        new Image.asset(
-                          "assets/Logo.png",
-                          //alignment: Alignment.center,
-                        )
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(top: 6),
+                              child: new Image.asset(
+                                "assets/Logo.png",
+                                width: 30,
+                                height: 30,
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.only(top: 25),
+                    alignment: Alignment.bottomLeft,
                     child: Column(
                       children: <Widget>[
                         new Image.asset(
                           "assets/Background.png",
-                          alignment: Alignment.center,
+                          fit: BoxFit.fitHeight,
+                          alignment: Alignment.bottomCenter,
                         )
                       ],
                     ),
